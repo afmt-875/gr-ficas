924a0fc972bde5419ce7822b042bc9efe07ac797
afmt-875 <a01704728@tec.mx>
2024-04-17 19:19:17 -0600

    Nombre

--- hola.py
+++ hola.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #creamos título de la App
 st.title("Titanic App")
-st.header("Titanic Graphs - App")
+st.header("Antonio Millán - A01704728")
 st.write("Gráficas del dataset Titanic")
 
 titanic_link = 'titanic 2.csv'
