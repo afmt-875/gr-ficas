f2c72441c175adb7cf8fed216999902fcd5022b6
afmt-875 <a01704728@tec.mx>
2024-04-17 19:28:19 -0600

    Graficas

--- hola.py
+++ hola.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib as plt
 #creamos título de la App
 st.title("Titanic App")
 st.header("Antonio Millán - A01704728")
@@ -8,3 +9,8 @@ st.write("Gráficas del dataset Titanic")
 titanic_link = 'titanic 2.csv'
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots()
+sx.hist(titanic_data.fare)
+st.header("Histograma del Titanic")
+st.pyplot(fig)
