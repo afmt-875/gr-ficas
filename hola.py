f266e88f88402ddda8456be1be5d567576d3044b
afmt-875 <a01704728@tec.mx>
2024-04-22 17:58:08 -0600

    Barras

--- hola.py
+++ hola.py
@@ -14,3 +14,14 @@ fig, ax = plt.subplots()
 ax.hist(titanic_data['Fare'])
 st.header("Histograma del Titanic")
 st.pyplot(fig)
+
+st.markdown("____")
+
+fig2, ax2 = plt.subplots()
+y_pos = titanic_data['Pclass']
+x_pos = titanic_data['Fare']
+ax2.barh(y_pos, x_pos)
+ax2.set_ylabel('Pclass')
+ax2.set_ylabel('Fare')
+st.header("Grafica de Barras del Titanic")
+st.pyplot(fig2)
