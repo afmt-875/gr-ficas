fa71535e65121f7e3095aee33aba22c2c7af3fef
afmt-875 <a01704728@tec.mx>
2024-04-22 18:18:33 -0600

    Final

--- hola.py
+++ hola.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 #creamos título de la App
 st.title("Titanic App")
 st.header("Antonio Millán - A01704728")
@@ -25,3 +26,39 @@ ax2.set_ylabel('Pclass')
 ax2.set_ylabel('Fare')
 st.header("Grafica de Barras del Titanic")
 st.pyplot(fig2)
+
+st.markdown("____")
+
+fig3, ax3 = plt.subplots()
+ax3.scatter(titanic_data['Age'], titanic_data['Fare'])
+ax3.set_ylabel('Edad')
+ax3.set_ylabel('Tarifa')
+st.header("Grafica de Dispersión del Titanic")
+st.pyplot(fig3)
+
+st.markdown("____")
+
+fig4, ax4 = plt.subplots()
+ax4 = titanic_data.boxplot(['Age'])
+ax4.set_ylabel('Edad')
+st.header("Grafica de Cajas por Edad")
+st.pyplot(fig4)
+
+st.markdown("____")
+
+fig5,ax5 = plt.subplots()
+hist_class = np.histogram(titanic_data['Pclass'], bins = 3, range = (1,3))[0]
+
+labels = ['1','2','3']
+colors = ['tab:blue', 'tab:red', 'tab:green']
+explode = [0,0,0.2]
+
+ax5.pie(hist_class,
+        labels = labels,
+         colors = colors, 
+         autopct  = '%.0f%%',
+         explode = explode, 
+         shadow = True)
+st.header("Grafica de Pastel - Clase Social")
+st.pyplot(fig5)
+st.dataframe(hist_class)
